62e1f771cf092e60712a86cc0f283786e8409fbb
w1skasss <144696777+w1skasss@users.noreply.github.com>
2023-10-09 16:49:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+//1 шленова лепилова
+// function callCounter() {
+//   let count = 0;
+//   function counter() {
+//     count++;
+//     console.log('количество вызовов функции: ' + count);
+//   }
+//   return counter;
+// }
+// const myCounter = callCounter();
+// myCounter(); 
+// myCounter(); 
+// myCounter(); 
+// myCounter(); 
+// myCounter(); 
+
+//2
+// function callCounter() {
+//   let count = 10;
+//   function counter() {
+//     count--;
+//     console.log('Число: ' + count);
+//   }
+//   return counter;
+// }
+// const myCounter = callCounter();
+// myCounter();
+// myCounter();
+// myCounter();
+// myCounter();
+// myCounter();
+
+//3
+// function createCounter() {
+//   let count = 10;
+//   function counter() {
+//     if (count > 0) {
+//       count--;
+//       console.log('Число: ' + count);
+//     } else {
+//       console.log('Отсчет окончен');
+//     }
+//   }
+//   return counter;
+// }
+// const myCounter = createCounter();
+// myCounter(); 
+// myCounter(); 
+// myCounter(); 
+// myCounter(); 
+// myCounter(); 
+// myCounter(); 
+// myCounter(); 
+// myCounter(); 
+// myCounter(); 
+// myCounter(); 
+// myCounter(); 
+// myCounter(); 
+
+//1
+// 0
+
+//2
+// 0
+
+//1
+// 0123
+
+//2
+// 0123
+
+//3
+// 0123
